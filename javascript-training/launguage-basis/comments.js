82c54be276576c906f379c9e65de2ab07d019c3a
Jayachandra Reddy <jayachandraqa32@gmail.com>
2026-05-26 21:04:00 +0530

    Assignment 1

---
+++ javascript-training/launguage-basis/comments.js
@@ -0,0 +1,13 @@
+//console is aprint statement, when ever we want to print somethink, we are going to print this simple statement 
+console.log('Hello, world');
+
+function add(a,b) {
+    return a +b
+}
+
+/*
+This is a function that is going to help us calculate the sum of two different numbers 
+*@param (number) a - This is first number
+*@param (number) b - This is second number
+*@returns {number} The sum of a and b
+*/
---
+++ javascript-training/launguage-basis/first-js-program.js
@@ -0,0 +1,6 @@
+console.log("jaya, World");
+
+console.info("this is an error message");
+console.error("this is an error message");
+console.warn("this is an error message");
+
